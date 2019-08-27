5342cd35d02c70e8c67e0294fe94150e0ef69cc9
Gcom0 <giancarlo.comelli@gmail.com>
2019-08-27 20:48:07 +0000

    test

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,34 @@
 'use strict'
 
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
+// Open Nav
+
 (function() {
 
   let hamburger = {
